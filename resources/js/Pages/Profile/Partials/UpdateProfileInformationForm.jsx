2a3d36a6062b2f88d5b0253c65d1d34fdeb6bb02
15-y-nakamura <C2234@oic.jp>
2024-10-24 23:17:05 +0900

    プロフィール画面の項目追加

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -14,8 +14,10 @@ export default function UpdateProfileInformation({
 
     const { data, setData, patch, errors, processing, recentlySuccessful } =
         useForm({
-            name: user.name,
+            nickname: user.nickname,
             email: user.email,
+            birthday: user.birthday,
+            login_id: user.login_id, // ログインIDを初期値として設定
         });
 
     const submit = (e) => {
@@ -28,33 +30,48 @@ export default function UpdateProfileInformation({
         <section className={className}>
             <header>
                 <h2 className="text-lg font-medium text-gray-900">
-                    Profile Information
+                    プロフィール情報
                 </h2>
 
                 <p className="mt-1 text-sm text-gray-600">
-                    Update your account's profile information and email address.
+                アカウントのプロフィール情報、メールアドレス、ユーザID、生年月日を更新します。
                 </p>
             </header>
 
             <form onSubmit={submit} className="mt-6 space-y-6">
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
+                    <InputLabel htmlFor="login_id" value="ユーザID" />
 
                     <TextInput
-                        id="name"
+                        id="login_id"
+                        type="text"
                         className="mt-1 block w-full"
-                        value={data.name}
-                        onChange={(e) => setData('name', e.target.value)}
+                        value={data.login_id}
+                        onChange={(e) => setData('login_id', e.target.value)}
                         required
-                        isFocused
-                        autoComplete="name"
                     />
 
-                    <InputError className="mt-2" message={errors.name} />
+                    <InputError className="mt-2" message={errors.login_id} />
                 </div>
 
                 <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                <InputLabel htmlFor="nickname" value="ニックネーム" />
+
+                    <TextInput
+                        id="nickname"
+                        className="mt-1 block w-full"
+                        value={data.nickname}
+                        onChange={(e) => setData('nickname', e.target.value)}
+                        required
+                        isFocused
+                        autoComplete="nickname"
+                    />
+
+                    <InputError className="mt-2" message={errors.nickname} />
+                </div>
+
+                <div>
+                    <InputLabel htmlFor="email" value="メールアドレス" />
 
                     <TextInput
                         id="email"
@@ -69,31 +86,45 @@ export default function UpdateProfileInformation({
                     <InputError className="mt-2" message={errors.email} />
                 </div>
 
+                <div>
+                    <InputLabel htmlFor="birthday" value="生年月日" />
+
+                    <TextInput
+                        id="birthday"
+                        type="date"
+                        className="mt-1 block w-full"
+                        value={data.birthday}
+                        onChange={(e) => setData('birthday', e.target.value)}
+                        autoComplete="birthday"
+                    />
+
+                    <InputError className="mt-2" message={errors.birthday} />
+                </div>
+
                 {mustVerifyEmail && user.email_verified_at === null && (
                     <div>
                         <p className="mt-2 text-sm text-gray-800">
-                            Your email address is unverified.
+                            メールアドレスが確認されていません。
                             <Link
                                 href={route('verification.send')}
                                 method="post"
                                 as="button"
                                 className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                                Click here to re-send the verification email.
+                                メールの再送信はこちらをクリック
                             </Link>
                         </p>
 
                         {status === 'verification-link-sent' && (
                             <div className="mt-2 text-sm font-medium text-green-600">
-                                A new verification link has been sent to your
-                                email address.
+                                新しい確認リンクがメールアドレスに送信されました。
                             </div>
                         )}
                     </div>
                 )}
 
                 <div className="flex items-center gap-4">
-                    <PrimaryButton disabled={processing}>Save</PrimaryButton>
+                    <PrimaryButton disabled={processing}>保存</PrimaryButton>
 
                     <Transition
                         show={recentlySuccessful}
@@ -103,7 +134,7 @@ export default function UpdateProfileInformation({
                         leaveTo="opacity-0"
                     >
                         <p className="text-sm text-gray-600">
-                            Saved.
+                            保存されました。
                         </p>
                     </Transition>
                 </div>
