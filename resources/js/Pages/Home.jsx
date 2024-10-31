f4de9e89e3d8f152eceee3bae410bfb5116eaf83
15-y-nakamura <C2234@oic.jp>
2024-11-01 00:02:43 +0900

    change:dashbord→homeに変更

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
-export default function Dashboard() {
+export default function Home() {
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    ダッシュボード
+                    ホーム
                 </h2>
             }
         >
-            <Head title="ダッシュボード" />
+            <Head title="ホーム画面" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
