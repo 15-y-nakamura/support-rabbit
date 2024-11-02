b5e0ca2457e52461425e11841e9748d23b678a3c
15-y-nakamura <C2234@oic.jp>
2024-11-03 00:24:08 +0900

    ヘッダーを作成し、ルートを /dashboard から /home に変更

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -7,166 +7,188 @@ import { useState } from "react";
 
 export default function AuthenticatedLayout({ header, children }) {
     const user = usePage().props.auth.user;
-
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
 
+    const toggleNavigationDropdown = () => {
+        setShowingNavigationDropdown((prevState) => !prevState);
+    };
+
     return (
         <div className="min-h-screen bg-gray-100">
-            <nav className="border-b border-gray-100 bg-white">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div className="flex h-16 justify-between">
+            <nav className="border-b border-gray-100 bg-customPink">
+                <div className="mx-auto max-w-7xl px-6 sm:px-8 lg:px-10">
+                    <div className="flex h-24 justify-between">
                         <div className="flex">
                             <div className="flex shrink-0 items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                <Link href="/home">
+                                    <img
+                                        src="/img/logo.png"
+                                        alt="Logo"
+                                        className="block h-16 w-auto fill-current text-gray-800"
+                                    />
+                                </Link>
+                                <Link
+                                    href="/home"
+                                    className="ml-4 text-xl text-gray-800"
+                                >
+                                    ホーム
                                 </Link>
                             </div>
-
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route("home")}
-                                    active={route().current("home")}
-                                >
-                                    HOME
-                                </NavLink>
-                            </div>
                         </div>
-                        <div className="hidden sm:ms-6 sm:flex sm:items-center">
-                            <div className="relative ms-3">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
-                                            >
-                                                {user.name}
-
-                                                <svg
-                                                    className="-me-0.5 ms-2 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link
-                                            href={route("profile.edit")}
-                                        >
-                                            Profile
-                                        </Dropdown.Link>
-                                        <Dropdown.Link
-                                            href={route("logout")}
-                                            method="post"
-                                            as="button"
-                                        >
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
+                        <div className="hidden sm:ms-8 sm:flex sm:items-center">
+                            <div className="flex items-center space-x-8 sm:space-x-6">
+                                <NavItem
+                                    href="/schedule"
+                                    icon="/img/schedule-icon.png"
+                                    label="本日の予定"
+                                />
+                                <Divider />
+                                <NavItem
+                                    href="/weather"
+                                    icon="/img/weather-icon.png"
+                                    label="天気"
+                                />
+                                <Divider />
+                                <NavItem
+                                    href="/connection"
+                                    icon="/img/connection-icon.png"
+                                    label="接続"
+                                    className="ms-8"
+                                />
+                                <Divider />
                             </div>
+                            <UserDropdown user={user} />
                         </div>
                         <div className="-me-2 flex items-center sm:hidden">
                             <button
-                                onClick={() =>
-                                    setShowingNavigationDropdown(
-                                        (previousState) => !previousState
-                                    )
-                                }
-                                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
+                                onClick={toggleNavigationDropdown}
+                                className="inline-flex items-center justify-center rounded-md p-3 text-gray-800 transition duration-150 ease-in-out focus:outline-none"
                             >
-                                <svg
-                                    className="h-6 w-6"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        className={
-                                            !showingNavigationDropdown
-                                                ? "inline-flex"
-                                                : "hidden"
-                                        }
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={
-                                            showingNavigationDropdown
-                                                ? "inline-flex"
-                                                : "hidden"
-                                        }
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
+                                <DropdownIcon />
                             </button>
                         </div>
                     </div>
                 </div>
 
-                <div
-                    className={
-                        (showingNavigationDropdown ? "block" : "hidden") +
-                        " sm:hidden"
-                    }
-                >
-                    <div className="space-y-1 pb-3 pt-2">
-                        <ResponsiveNavLink
-                            href={route("home")}
-                            active={route().current("home")}
-                        ></ResponsiveNavLink>
-                    </div>
-
-                    <div className="border-t border-gray-200 pb-1 pt-4">
-                        <div className="px-4">
-                            <div className="text-base font-medium text-gray-800">
-                                {user.name}
-                            </div>
-                            <div className="text-sm font-medium text-gray-500">
-                                {user.email}
-                            </div>
-                        </div>
-
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route("profile.edit")}>
-                                Profile
-                            </ResponsiveNavLink>
-                            <ResponsiveNavLink
-                                method="post"
-                                href={route("logout")}
-                                as="button"
-                            >
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
+                <ResponsiveNavigation showing={showingNavigationDropdown} />
             </nav>
 
             {header && (
                 <header className="bg-white shadow">
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <div className="mx-auto max-w-7xl px-6 py-8 sm:px-8 lg:px-10">
                         {header}
                     </div>
                 </header>
             )}
 
             <main>{children}</main>
+        </div>
+    );
+}
+
+function NavItem({ href, icon, label, className }) {
+    return (
+        <Link
+            href={href}
+            className={`text-lg text-gray-800 flex flex-col items-center ${className}`}
+        >
+            <img src={icon} alt={`${label} Icon`} className="h-6 w-6 mb-1" />
+            {label}
+        </Link>
+    );
+}
+
+function Divider() {
+    return <div className="h-6 border-l-2 border-white"></div>;
+}
+
+function UserDropdown({ user }) {
+    return (
+        <div className="relative ms-6">
+            <Dropdown>
+                <Dropdown.Trigger>
+                    <span className="inline-flex rounded-md">
+                        <button
+                            type="button"
+                            className="inline-flex items-center rounded-md bg-transparent px-4 py-3 text-lg leading-5 text-gray-800 transition duration-150 ease-in-out focus:outline-none"
+                        >
+                            {user.name}
+                            <DropdownIcon />
+                        </button>
+                    </span>
+                </Dropdown.Trigger>
+                <Dropdown.Content>
+                    <Dropdown.Link href={route("profile.edit")}>
+                        Profile
+                    </Dropdown.Link>
+                    <Dropdown.Link
+                        href={route("logout")}
+                        method="post"
+                        as="button"
+                    >
+                        Log Out
+                    </Dropdown.Link>
+                </Dropdown.Content>
+            </Dropdown>
+        </div>
+    );
+}
+
+function DropdownIcon() {
+    return (
+        <div className="flex flex-col items-center ms-2">
+            <img
+                src="/img/dropdown-icon.png"
+                alt="Dropdown Icon"
+                className="h-6 w-6 mb-1"
+            />
+            <span className="text-lg text-gray-800 flex flex-col items-center">
+                メニュー
+            </span>
+        </div>
+    );
+}
+
+function ResponsiveNavigation({ showing }) {
+    return (
+        <div className={(showing ? "block" : "hidden") + " sm:hidden"}>
+            <div className="space-y-2 pb-4 pt-3">
+                <ResponsiveNavLink
+                    href="/schedule"
+                    className="text-lg text-gray-800 flex flex-col items-center"
+                >
+                    <img
+                        src="/img/schedule-icon.png"
+                        alt="Schedule Icon"
+                        className="h-6 w-6 mb-1"
+                    />
+                    本日の予定
+                </ResponsiveNavLink>
+                <ResponsiveNavLink
+                    href="/weather"
+                    className="text-lg text-gray-800 flex flex-col items-center"
+                >
+                    <img
+                        src="/img/weather-icon.png"
+                        alt="Weather Icon"
+                        className="h-6 w-6 mb-1"
+                    />
+                    天気
+                </ResponsiveNavLink>
+                <div className="mt-4 space-y-2">
+                    <ResponsiveNavLink href={route("profile.edit")}>
+                        Profile
+                    </ResponsiveNavLink>
+                    <ResponsiveNavLink
+                        method="post"
+                        href={route("logout")}
+                        as="button"
+                    >
+                        Log Out
+                    </ResponsiveNavLink>
+                </div>
+            </div>
         </div>
     );
 }
